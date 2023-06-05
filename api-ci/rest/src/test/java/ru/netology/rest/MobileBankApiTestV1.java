b1e734420635bf284ac70ca185e29f72cc08eeb4
Irina.Test <ira.test2022@gmail.com>
2023-06-05 09:32:16 +0300

    Changes api-ci

--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,6 +1,11 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import  static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 
 import static io.restassured.RestAssured.given;
 
@@ -17,5 +22,70 @@ class MobileBankApiTestV1 {
       // Проверки
       .then()
           .statusCode(200);
+    }
+    @Test
+    void shouldReturnDemoAccounts1() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .contentType(ContentType.JSON);
+    }
+    @Test
+    void shouldReturnDemoAccounts2() { // проверка что 3 поля
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("",hasSize(3));
+    }
+    @Test
+    void shouldReturnDemoAccounts3() { // проверка валюты доллар
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[1].currency",equalTo("USD"));
+    }
+    @Test
+    void shouldReturnDemoAccounts4() { // проверка положительного баланса
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body(" every { it.balance >= 0 }", is(true));
+    }
+    @Test
+    void shouldReturnDemoAccounts5() { // проверка валидации полей
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
